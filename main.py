0000c4d8f0c584211daf5ecd5673b64124e380fa
tlgserageldin <tserageldin@protonmail.com>
2025-06-24 18:42:02 -0600

    dont need to launch frontend, just try and fetch the partnumber

--- main.py
+++ main.py
@@ -14,4 +14,5 @@ if __name__ == "__main__":
 
     backend = Backend(ledControl)
 
-    Frontend(backend, digikeyAPI, ledControl)
+#    Frontend(backend, digikeyAPI, ledControl)
+    digikeyAPI.fetch_media('311-1.00CRCT-ND')
