a9bb0e3364fb5461ba125fdc38fb43c309f7495b
tlgserageldin <tserageldin@protonmail.com>
2025-06-27 11:01:46 -0600

    Added enter, exit, del, methods, made conn part of init and fixed opening and closing connections every method instead of cursors.

--- image_cache.py
+++ image_cache.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 @dataclass
 class ImageCacheEntry:
-    part_number: str | None
+    dk_part_number: str | None
     image: bytes | None
     etag: str | None
     fetched_at: datetime | None
@@ -14,45 +14,84 @@ class Image_Cache:
     def __init__(self):
         # guaranteed to be created
         self.db_file = os.path.join(os.path.dirname(__file__), "Databases", "image_cache.db") 
+        self.conn = sqlite3.connect(self.db_file)
+
+    '''
+    # enter and exit are for use in with blocks, e.g.
+    with Image_Cache() as cache:
+        if not cache.already_exists("...."):
+            cache.store_blob(entry)
+    '''
+
+    def __enter__(self): 
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.conn:
+            self.conn.close()
+
+    def __del__(self):
+        try:
+            if self.conn:
+                self.conn.close()
+        except Exception:
+            pass # avoid crash
+
+
+    def print_entry(self, part_number: str | None):
+        cursor = self.conn.cursor()
+        cursor.execute("""
+            SELECT part_number, image, etag, fetched_at 
+            FROM image_cache WHERE part_number = ?
+            """, (part_number,)) # single comma to make it a tuple
+        entry = cursor.fetchone()
 
     def already_exists(self, part_number: str | None):
-        conn = sqlite3.connect(self.db_file)
-        cursor = conn.cursor()
-
-        cursor.execute("SELECT 1 FROM image_cache WHERE part_number = ?", (part_number,)) # single comma to make it a tuple
+        cursor = self.conn.cursor()
+        cursor.execute("""
+            SELECT 1
+            FROM image_cache 
+            WHERE part_number = ?
+        """, (part_number,)) # single comma to make it a tuple
+        cursor.close()
         found = cursor.fetchone() is not None
-        conn.close()
         return found
 
-    def request_cache(self, part_number: str | None):
-        if not part_number:
+    def request_entry(self, dk_part_number: str | None):
+        if not dk_part_number:
             return None
 
-        conn = sqlite3.connect(self.db_file)
-        cursor = conn.cursor()
-
-        cursor.execute("SELECT 1 FROM image_cache WHERE part_number = ?", (part_number,)) # single comma to make it a tuple
+        cursor = self.conn.cursor()
+        cursor.execute("""
+            SELECT part_number, image, etag, fetched_at
+            FROM image_cache 
+            WHERE part_number = ?
+        """, (dk_part_number,)) # single comma to make it a tuple
         row = cursor.fetchone()
-        conn.close()
+        cursor.close()
 
         if row:
             return ImageCacheEntry(
-                part_number=row[0],
+                dk_part_number=row[0],
                 image=row[1],
                 etag=row[2],
-                fetched_at=datetime.fromisoformat(row[4])
+                fetched_at=datetime.fromisoformat(row[3])
             )
         return None
 
-
-        
-
     def store_blob(self, entry: ImageCacheEntry):
-        if self.already_exists(entry.part_number):
-            conn = sqlite3.connect(self.db_file)
-            cursor = conn.cursor()
+        cursor = self.conn.cursor()
+        if self.already_exists(entry.dk_part_number):
             cursor.execute("""
                UPDATE image_cache
                SET image = ?, etag = ?, fetched_at = CURRENT_TIMESTAMP
                WHERE part_number = ?
-            """, (entry.image, entry.etag, entry.part_number))
+            """, (entry.image, entry.etag, entry.dk_part_number))
+        else:
+            cursor.execute("""
+                INSERT INTO image_cache (part_number, image, etag, fetched_at)
+                VALUES (?, ?, ?, ?)
+            """, (entry.dk_part_number, entry.image, entry.etag, entry.fetched_at))
+        self.conn.commit()
+        cursor.close()
+
