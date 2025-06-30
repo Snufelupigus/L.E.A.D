bed032e99db3f3d23075a36c03d197479d5bbd78
tlgserageldin <tserageldin@protonmail.com>
2025-06-30 10:57:02 -0600

    Updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from file_initializer import FileInitializer
 from backend import Backend
 from frontend import Frontend
+from image_cache import Image_Cache
 from digikey_api_local import Digikey_API_Call
 from ledSerial import LedController
 
@@ -11,7 +12,8 @@ initializer.initialize_files()
 if __name__ == "__main__":
     digikeyAPI = Digikey_API_Call()
     ledControl  = LedController()
+    imageCache = Image_Cache()
 
     backend = Backend(ledControl)
 
-    Frontend(backend, digikeyAPI, ledControl)
+    Frontend(backend, digikeyAPI, ledControl, imageCache)
